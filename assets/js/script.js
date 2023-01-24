36cdc8b87773d97947f203c436efbb3fc38d4aa6
LinnSilver <baralinn@gmail.com>
2023-01-24 14:06:13 +0000

    Add JS rule to add checkbox and  task from inputfeald to list,

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,39 @@
+//Start counting for id number on list task.
+var todoNr = 0;
 
-//Listen for click createTaskButton to create list ithem
+//Listen for click createTaskButton to create list ithem.
 document.getElementById("createTaskButton").addEventListener("click", () => {
     createListIthem();
 });
 
-//Listen for enter key up to create list item
+//Listen for enter key up to create list item.
 let newTask = document.getElementById("newInput");
 newTask.addEventListener("keyup", (event) => {
     if(event.key === "Enter"){
         createListIthem();
     }
 })
+
+//Add task to list. 
+function createListIthem(){
+
+    //Add id number to list task
+    todoNr++;
+    let Input = "newInput";
+    let newTask = document.getElementById(Input).value;
+
+    //Prevent empty input.
+    if (newTask.trim() === ""){
+        alert("Input field is empty. Please write something!");
+        return false;
+    }
+
+    let newListIthem = document.createElement("li");
+    newListIthem.setAttribute("id", "todo_" + todoNr);
+    newListIthem.innerHTML = '<input type = "checkbox"  onClick="toDoDone(' + todoNr + ')">' + newTask;
+    console.log(newListIthem);
+    document.getElementById("todo").appendChild(newListIthem);
+
+    //clears inputfeald after adding to list.
+    document.getElementById(Input).value = "";
+}
